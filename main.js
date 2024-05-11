d744e9eb45e4bf943385d201a44de140558649f7
Mediahuella <sebastian@mediahuella.com>
2024-05-11 02:56:51 -0400

    first commit

--- main.js
+++ main.js
@@ -1,13 +1,9 @@
 import './style.css'
 
 document.addEventListener('DOMContentLoaded', ()=> {
-  //Escribir acá las funciones para el funcionamiento
+  // 1.Crear la funcionalidad para abrir con el botón de menú, las clases para abrir y cerrar ya están hechas en el css.
 
-  // 1.Crear la funcionalidad para abrir con el botón de menú, las clases de abrir
-  // para abrir y cerrar ya están hechas en el css.
-
-  // 2.Generar un botón que esté la esquina superior derecha del drawer menu
-  // para que cierre el menú.
+  // 2.Generar un botón que esté la esquina superior derecha del drawer menu para que cierre.
 
   // 3. Hacer que el menu cierre al presionar el overlay.
 })
